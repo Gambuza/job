654ca4d599280de1f3b235f25b1b5fdbd1db29c0
Gambuza <tapochamka@gmail.com>
2016-01-20 19:41:29 +0400

    new

---
+++ dist/js/ul.js
@@ -0,0 +1,38 @@
+// Карусель
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem : true,
+      autoPlay : 3000,
+      pagination : true,
+  });
+ 
+});
+// Модальное окно
+$(document).ready(function() {
+  $('a[name=modal]').click(function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    $('#mask').css({'width':maskWidth,'height':maskHeight});
+    $('#mask').fadeIn(1000);
+    $('#mask').fadeTo("slow",0.8);
+    var winH = $(window).height();
+    var winW = $(window).width();
+    $(id).css('top',  winH/0.5-$(id).height()/2);
+    $(id).css('left', winW/2-$(id).width()/2);
+    $(id).fadeIn(2000);
+  });
+  $('.window .close').click(function (e) {
+    e.preventDefault();
+    $('#mask, .window').hide();
+  });
+  $('#mask').click(function () {
+    $(this).hide();
+    $('.window').hide();
+  });
+});
