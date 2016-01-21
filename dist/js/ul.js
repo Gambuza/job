a87ebbc008a7643105a15a5975daedad0490223f
Gambuza <tapochamka@gmail.com>
2016-01-21 07:57:11 +0400

    сделал слайдер и начал блок с акциями,много времени потратил на слайдер,еще и переделывал,так как коппеляция испоганилась) да и так по мелочи,проснусь продолжу

--- dist/js/ul.js
+++ dist/js/ul.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
  
 $('.owl-carousel').owlCarousel({
     loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
     responsive:{
         0:{
             items:1
